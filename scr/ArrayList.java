f4a2bf580974da39e2209959576276d5e4c07904
gauravnavghare <gn2618pbn1@gmail.com>
2023-08-18 22:26:52 +0530

    array.

---
+++ scr/ArrayList.java
@@ -0,0 +1,26 @@
+
+import java.util.ArrayList;
+
+public class TestApp {
+    public static void main(String[] args) {
+        int[] elements = new int[2];
+        elements[0] = 10;
+        elements[1] = 20;
+
+        ArrayList arrayList = new ArrayList();
+        arrayList.add(10);
+        arrayList.add(20);
+        arrayList.add("hello");
+        String hello = "hello";
+
+        ArrayList<Integer> intData = new ArrayList<>();
+        intData.add(10);
+        intData.add(20);
+//        intData.add("hello"); // gives compilation error
+
+        for(int i = 0; i < intData.size(); i++){
+            System.out.println(intData.get(i));
+        }
+
+    }
+}
